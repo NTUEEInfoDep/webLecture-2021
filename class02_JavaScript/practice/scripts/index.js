81aeebc7a09103297b4fb1d35a9f3d3fdd924c42
YianLai0327 <ianlai0327@gmail.com>
2021-10-29 19:11:58 +0800

    Initial commit

--- class02_JavaScript/practice/scripts/index.js
+++ class02_JavaScript/practice/scripts/index.js
@@ -1,4 +1,6 @@
-const btnLike = document.getElementById("btn_like");
+const btnLike = document.getElementById("btn-like");
+const btnHate = document.getElementById("btn-hate");
+const btnReset = document.getElementById("btn-reset");
 // Todo:
 // Find and declare the button elements (hate/reset) to add onclick function.
 
@@ -6,10 +8,26 @@ const text = document.getElementById("text");
 
 let count = 0;
 
-btnLike.onclick = () => {
+function LikeHandler() {
   count += 1;
-  text.innerText = `Like: ${count.toString()}`;
-};
+  //text.innerText = `Like: ${count.toString()}`;
+  text.innerText = "Like: " + count.toString();
+}
+function HateHandler() {
+  count -= 1;
+  text.innerText = "Like: " + count.toString();
+  if (count < 0)
+    text.innerText = "Like: " + count.toString() + " \\Too much hatred!!";
+}
+function ResetHandler() {
+  count = 0;
+  text.innerText = "Like: " + count.toString();
+  alert("Be careful to be nice!!");
+}
+
+btnLike.onclick = LikeHandler;
+btnHate.onclick = HateHandler;
+btnReset.onclick = ResetHandler;
 
 // Todo:
 // Define the onclick function for each button (hate/reset)
